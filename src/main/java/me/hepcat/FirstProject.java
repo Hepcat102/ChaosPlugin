abef0ca3e1e4efa9e42959ba41ff92fead43b1b3
BuildTools <unconfigured@null.spigotmc.org>
2021-04-25 13:49:27 -0700

    opimise

--- src/main/java/me/hepcat/FirstProject.java
+++ src/main/java/me/hepcat/FirstProject.java
@@ -2,19 +2,12 @@ package me.hepcat;
 
 import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.entity.EntityBreakDoorEvent;
-import org.bukkit.event.entity.EntityToggleGlideEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerToggleFlightEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.Objects;
 
 public class FirstProject extends JavaPlugin implements Listener {
 
