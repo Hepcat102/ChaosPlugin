f1f20f5ae62e3a269d0691e6e96ba39cd6e11d14
BuildTools <unconfigured@null.spigotmc.org>
2021-04-25 13:52:33 -0700

    opimise

--- src/main/java/me/hepcat/FirstProject.java
+++ src/main/java/me/hepcat/FirstProject.java
@@ -9,6 +9,8 @@ import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public class FirstProject extends JavaPlugin implements Listener {
 
     public void onEnable() {
@@ -23,7 +25,7 @@ public class FirstProject extends JavaPlugin implements Listener {
     public void BoomHit(ProjectileHitEvent e) {
         Block b = e.getHitBlock();
         if (b == null)
-            e.getHitEntity().getWorld().createExplosion(e.getHitEntity().getLocation(), 5, false);
+            Objects.requireNonNull(e.getHitEntity()).getWorld().createExplosion(e.getHitEntity().getLocation(), 5, false);
         else {
             e.getHitBlock().getWorld().createExplosion(e.getHitBlock().getLocation(), 5, false);
         }
